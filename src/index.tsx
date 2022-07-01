9cd1ab055b490519704eefde9823102f9cb49288
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-07-01 13:27:20 +0300

    change the path to normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./normalize.scss";
+import "./assets/normalize.scss";
 import { App } from "./app";
 
 const rootElement = document.getElementById("root") as HTMLDivElement;
